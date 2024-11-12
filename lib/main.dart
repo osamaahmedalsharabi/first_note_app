41b65a15d35584b1dfe69f14143194063c2b2ad9
osamaahmedalsharabi <osamaahmed70088@gmail.com>
2024-11-12 12:52:34 +0300

    add file settings.json and create note view file and change theme app

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
+import 'package:first_note_app/views/note_view.dart';
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(const NoteApp());
-} 
+}
 
 class NoteApp extends StatelessWidget {
   const NoteApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: const NoteView(),
+    );
   }
 }
