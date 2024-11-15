1e54d71619b68cd3675bd32e294f41e253f92494
osamaahmedalsharabi <osamaahmed70088@gmail.com>
2024-11-15 16:18:59 +0300

    add addNote to modal Buttom sheet

--- lib/cubits/add_note_cubit/add_note_cubit_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit_cubit.dart
@@ -1,11 +1,22 @@
+import 'package:first_note_app/constants.dart';
 import 'package:first_note_app/models/note_model.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 part 'add_note_cubit_state.dart';
 
 class AddNoteCubitCubit extends Cubit<AddNoteCubitState> {
   AddNoteCubitCubit() : super(AddNoteCubitInitial());
 
-  addNote(NoteModel note){
-    
+  addNote(NoteModel note) {
+    emit(AddNoteCubitLoading());
+    Future.delayed(const Duration(seconds: 2), () async {
+      try {
+        var noteBox = Hive.box<NoteModel>(kNoteBox);
+        await noteBox.add(note);
+        emit(AddNoteCubitSuccesed());
+      } catch (e) {
+        emit(AddNoteCubitFailure(errorMessage: e.toString()));
+      }
+    });
   }
 }
--- lib/views/widgets/custom_model_buttom_sheet_widget.dart
+++ lib/views/widgets/custom_model_buttom_sheet_widget.dart
@@ -1,5 +1,10 @@
+import 'dart:developer';
+
+import 'package:first_note_app/cubits/add_note_cubit/add_note_cubit_cubit.dart';
 import 'package:first_note_app/views/widgets/custom_form_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class CustomModelButtomSheetWidget extends StatelessWidget {
   const CustomModelButtomSheetWidget({
@@ -10,10 +15,23 @@ class CustomModelButtomSheetWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: MediaQuery.sizeOf(context).height * 0.5,
-      child: const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 16),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16),
         child: SingleChildScrollView(
-          child: CustomFormWIdget(),
+          child: BlocConsumer<AddNoteCubitCubit, AddNoteCubitState>(
+            listener: (context, state) {
+              if (state is AddNoteCubitFailure) {
+                log(state.errorMessage.toString());
+              } else {
+                Navigator.pop(context);
+              }
+            },
+            builder: (context, state) {
+              return ModalProgressHUD(
+                  inAsyncCall: state is AddNoteCubitLoading ? true : false,
+                  child: const CustomFormWIdget());
+            },
+          ),
         ),
       ),
     );
